cd1b15092b3d00cc9f9c1a5ed5a68df59ee77469
Fide <72738638+fidelp27@users.noreply.github.com>
2024-08-15 12:24:29 +0000

    traffic ligths

---
+++ trafficLigths.js
@@ -0,0 +1,18 @@
+
+/*
+You're writing code to control your town's traffic lights. You need a function to handle each change from green, to yellow, to red, and then to green again.
+
+Complete the function that takes a string as an argument representing the current state of the light and returns a string representing the state the light should change to.
+
+For example, when the input is green, output should be yellow
+*/
+function updateLight(current) {
+  let colors = ['green', 'yellow', 'red']
+  //your code here!
+  let findIndexColor = colors.findIndex((elem)=>elem === current.toLowerCase())
+  if(findIndexColor === colors.length -1){
+    return colors[0]
+  }
+  return colors[findIndexColor + 1]
+
+}
